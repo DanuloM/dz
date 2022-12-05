62e944fab7d963eb2421fd0359a1cea3e921ab39
Danulo.m <danulo1337@gmail.com>
2022-12-05 23:40:44 +0200

    last commit, result

--- Project65/Source.cpp
+++ Project65/Source.cpp
@@ -1,1 +1,173 @@
-﻿// commit 4
+﻿#include<iostream>
+#include <vector>
+using namespace std;
+class point {
+	int x;
+	int y;
+public:
+	point() {
+		x = 0;
+		y = 0;
+	}
+	point(int x, int y) {
+		this->x = x;
+		this->y = y;
+	}
+	void set() {
+		cout << "Enter X: ";
+		cin >> x;
+		cout << "Enter Y: ";
+		cin >> y;
+	}
+	void setRand() {
+		x = rand() % 10 + 1;
+		y = rand() % 10 + 1;
+	}
+	void set(int xValue, int yValue) {
+		x = xValue;
+		y = yValue;
+	}
+
+	void setX(int xValue) {
+		x = xValue;
+	}
+
+	void setY(int yValue) {
+		y = yValue;
+	}
+	//Методи - інспектори 
+	int getX() {
+		return x;
+	}
+	int getY() {
+		return y;
+	}
+
+
+	void print() {
+		cout << "X: " << x << "\nY: " << y << "\n";
+	}
+
+};
+
+class Figure {
+public:
+	virtual void print() = 0;
+};
+
+class rectangle : public Figure {
+	point points[4];
+public:
+	void set() {
+		for (int i = 0; i < 4; i++)
+			points[i].set();
+	}
+	void setR() {
+		for (int i = 0; i < 4; i++)
+			points[i].setRand();
+	}
+	void print() {
+		for (int i = 0; i < 4; i++) {
+			cout << "Point: " << i + 1 << endl;
+			points[i].print();
+		}
+	}
+};
+class triangle : public Figure {
+	point points[3];
+public:
+	void set() {
+		for (int i = 0; i < 3; i++)
+			points[i].set();
+	}
+	void setR() {
+		for (int i = 0; i < 3; i++)
+			points[i].setRand();
+	}
+	void print() {
+		for (int i = 0; i < 3; i++) {
+			cout << "Point: " << i + 1 << endl;
+			points[i].print();
+		}
+	}
+};
+
+class diamond : public Figure {
+	point points[4];
+public:
+	void set() {
+		for (int i = 0; i < 4; i++)
+			points[i].set();
+	}
+	void setR() {
+		for (int i = 0; i < 4; i++)
+			points[i].setRand();
+	}
+	void print() {
+		for (int i = 0; i < 4; i++) {
+			cout << "Point: " << i + 1 << endl;
+			points[i].print();
+		}
+	}
+};
+
+class quadrangle : public Figure {
+	point points[4];
+public:
+	void set() {
+		for (int i = 0; i < 4; i++)
+			points[i].set();
+	}
+	void setR() {
+		for (int i = 0; i < 4; i++)
+			points[i].setRand();
+	}
+	void print() {
+		for (int i = 0; i < 4; i++) {
+			cout << "Point: " << i + 1 << endl;
+			points[i].print();
+		}
+	}
+};
+int main() {
+	srand(time(0));
+
+	diamond d[2];
+	for (int i = 0; i < 2; i++) {
+		d[i].setR();
+	}
+	for (int i = 0; i < 2; i++) {
+		cout << endl << "Diamond: " << i + 1 << endl;
+		d[i].print();
+	}
+
+	quadrangle q[2];
+	for (int i = 0; i < 2; i++) {
+		q[i].setR();
+	}
+	for (int i = 0; i < 2; i++) {
+		cout << endl << "Quadrangle: " << i + 1 << endl;
+		q[i].print();
+	}
+
+	rectangle r[2];
+	for (int i = 0; i < 2; i++) {
+		r[i].setR();
+	}
+	for (int i = 0; i < 2; i++) {
+		cout << endl << "Rectangle: " << i + 1 << endl;
+		r[i].print();
+	}
+
+	triangle t[2];
+	for (int i = 0; i < 2; i++) {
+		t[i].setR();
+	}
+	for (int i = 0; i < 2; i++) {
+		cout << endl << "Triangle: " << i + 1 << endl;
+		t[i].print();
+	}
+
+
+	return 0;
+}
